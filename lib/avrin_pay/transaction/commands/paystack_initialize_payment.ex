e35667fb3c0fa568ccdafd388ecaa609955c901a
isaaccleopas <94404627+isaaccleopas@users.noreply.github.com>
2024-07-12 15:08:49 +0100

    chore: update paystack

--- lib/avrin_pay/transaction/commands/paystack_initialize_payment.ex
+++ lib/avrin_pay/transaction/commands/paystack_initialize_payment.ex
@@ -5,21 +5,35 @@ defmodule AvrinPay.Transaction.Commands.PaystackInitializePayment do
     attribute :payment_id, :uuid, allow_nil?: false, primary_key?: true, default: &Ash.UUID.generate/0
     attribute :amount, :integer, allow_nil?: false, description: "The amount in kobo"
     attribute :email, :string, allow_nil?: false
+    attribute :authorization_url, :string, allow_nil?: true
   end
 
   actions do
-    default_accept [:payment_id, :amount, :email]
+    default_accept [:payment_id, :amount, :email, :authorization_url]
     defaults [:create, :read]
 
     create :dispatch do
+      change fn changeset, _context ->
+        # Access email and amount from changeset data
+        email = Ash.Changeset.get_argument_or_attribute(changeset, :email)
+        amount = Ash.Changeset.get_argument_or_attribute(changeset, :amount)
+
+        case Paystack.Transaction.initialize(%{email: email, amount: amount}) do
+          {:ok, %Paystack.Response{data: %{"authorization_url" => authorization_url}}} ->
+            Ash.Changeset.change_attribute(changeset, :authorization_url, authorization_url)
+
+          _ ->
+            Ash.Changeset.add_error(changeset, "Paystack API Error")
+        end
+      end
+
+      # After action to dispatch command
       change fn changeset, _context ->
         Ash.Changeset.after_action(changeset, fn changeset, command ->
           case AvrinPay.Setup.Application.dispatch(command) do
             :ok -> {:ok, command}
             {:error, error} -> {:error, error}
           end
-
-          {:ok, command}
         end)
       end
     end
