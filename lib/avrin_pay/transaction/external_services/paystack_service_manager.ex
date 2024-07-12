e7f5e890d7cd030a3968aa48b163d75ca0948cca
isaaccleopas <94404627+isaaccleopas@users.noreply.github.com>
2024-07-12 09:43:38 +0100

    chore: return paystack service manager to the previous

--- lib/avrin_pay/transaction/external_services/paystack_service_manager.ex
+++ lib/avrin_pay/transaction/external_services/paystack_service_manager.ex
@@ -10,16 +10,9 @@ defmodule AvrinPay.Transaction.ExternalServices.PaystackServiceManager do
     :ok
   end
 
-  def initialize(email, amount, opts \\ []) do
-    callback_url = Keyword.get(opts, :callback_url, nil)
-    params = %{
-      email: email,
-      amount: amount
-    }
-    |> Map.put_new(:callback_url, callback_url)
-
+  def initialize(email, amount) do
     unless Application.get_env(:avrin_pay, :mock_api_call?) do
-      case Paystack.Transaction.initialize(params) do
+      case Paystack.Transaction.initialize(%{email: email, amount: amount}) do
         {:ok, %Paystack.Response{} = response} -> {:ok, response}
         {:error, error} -> {:error, error}
       end
