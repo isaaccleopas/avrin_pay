3b75cb041b7ad801f660ccc195aa899dc54693e9
isaaccleopas <94404627+isaaccleopas@users.noreply.github.com>
2024-07-15 11:58:24 +0100

    refactor: compare command and event fields

--- test/avrin_pay/transaction/paystack_test.exs
+++ test/avrin_pay/transaction/paystack_test.exs
@@ -7,14 +7,15 @@ defmodule AvrinPay.Transaction.PaystackTest do
 
   describe "Paystack" do
     test "GIVEN: a paystack initialize command is dispatched, WHEN: the command is executed, THEN: paystack payment is initialized" do
-      {:ok, %{payment_id: payment_id} = _command} = AvrinPay.Transaction.initialize_paystack_payment("example@gmail.com", 50000, "www.example.com")
+      {:ok, command} = AvrinPay.Transaction.initialize_paystack_payment("example@gmail.com", 50000, "www.example.com")
 
       assert_receive_event(Application, PaystackPaymentInitializedV1,
-        fn event -> event.payment_id == payment_id end,
+        fn event -> event.payment_id == command.payment_id end,
         fn event ->
-          assert event.email == "example@gmail.com"
-          assert event.amount == 50000
-          assert event.callback_url == nil
+          assert event.email == command.email
+          assert event.amount == command.amount
+          assert event.callback_url == command.callback_url
+          # assert event.paystack_authorization_url == command.paystack_authorization_url
         end
       )
     end
