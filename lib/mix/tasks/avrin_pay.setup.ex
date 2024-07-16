e4a9236f2ddb2a86fbf70b338a3cf2fb32f8e69d
isaaccleopas <94404627+isaaccleopas@users.noreply.github.com>
2024-07-16 08:08:24 +0100

    chore: update avrin_pay.setup.ex

--- lib/mix/tasks/avrin_pay.setup.ex
+++ lib/mix/tasks/avrin_pay.setup.ex
@@ -1,19 +1,17 @@
 defmodule Mix.Tasks.AvrinPay.Setup do
   use Mix.Task
-  alias AvrinPay.Setup.Repo
-  alias AvrinPay.Setup.EventStore
 
   @shortdoc "Sets up the AvrinPay application"
   def run(_args) do
     Mix.Task.run("app.start", [])
 
     # Create and migrate the repo
-    Mix.Task.run("ecto.create", ["-r", "AvrinPay.Setup.Repo"])
-    Mix.Task.run("ecto.migrate", ["-r", "AvrinPay.Setup.Repo"])
+    Mix.Task.run("ecto.create", ["--repo", "AvrinPay.Setup.Repo"])
+    Mix.Task.run("ecto.migrate", ["--repo", "AvrinPay.Setup.Repo"])
 
     # Create and init the event store
-    Mix.Task.run("event_store.create", ["-r", "AvrinPay.Setup.EventStore"])
-    Mix.Task.run("event_store.init", ["-r", "AvrinPay.Setup.EventStore"])
+    Mix.Task.run("event_store.create", ["--eventstore", "AvrinPay.Setup.EventStore"])
+    Mix.Task.run("event_store.init", ["--eventstore", "AvrinPay.Setup.EventStore"])
 
     # Any other setup tasks
     IO.puts("AvrinPay setup completed.")
